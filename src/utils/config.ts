25b0fac7d4c66805e4e1aa84d7fe835ff712ae74
noia <ohayo@discord-doesnt.work>
2024-07-27 21:20:24 +1000

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,3 @@
-/*
 const config = {
     token_secret: "35c8...",
     gateway: "",
@@ -18,27 +17,4 @@ const config = {
     key_path: "",
     ws_port: 0
 };
-export default config;
-*/
-
-const config = {
-    token_secret: "35c8...",
-    gateway: "", //Leave as blank for it to use the location ip (if one)
-    use_wss: true,
-    base_url: 'staging_2015.oldcordapp.com',
-    local_deploy: false,
-    use_same_port: true,
-    port: 1337,
-    cache404s: false,
-    gateway_has_no_port: true,
-    instance_name: "2015 Staging",
-    instance_description: "An oldcord instance",
-    instance_flags: [
-	"AUTOJOIN:1264743522297188359"
-    ],
-    cert_path: "", //if using wss, this needs to be specified
-    key_path: "", //i got lazy
-    ws_port: 0 //only need to specify if use_same_port is false 
-};
-
 export default config;
